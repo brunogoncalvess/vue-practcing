1929251c4536bb06bc6674de55374bd521e8a33e
unknown <brunogoncalves@outlook.com.br>
2022-11-23 23:33:05 -0300

    mais atividades

---
+++ vue-vite/src/components/UsersComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card 
+    class="card" 
+    :title="props.user.name" 
+    :text="props.user.email" 
+    variant="tonal"
+  ></v-card>
+</template>
+
+<script setup>
+const props = defineProps(['user'])
+</script>
+
+<style scoped>
+  .card {
+    margin: 10px;
+  }
+</style>
